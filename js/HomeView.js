1660641d83da6b65cc87ce7332d1b4ae1841aa88
gettuits <gettuits@gmail.com>
2013-08-28 12:02:46 +0200

    Creating a View Class

---
+++ js/HomeView.js
@@ -0,0 +1,32 @@
+// HomeView Class
+var HomeView = function(store) {
+	this.initialize = function() {
+    	// Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup', '.search-key', this.findByName);
+    };
+ 	
+
+    // Add home 
+    this.render: function(){
+        this.el.html(HomeView.template());
+    	return this;
+    };
+
+    //
+    this.findByName: function() {
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(HomeView.liTemplate));
+
+        });
+    };
+
+
+    this.initialize();
+ 
+} // Homeview 
+
+
+// Handlebars templates
+HomeView.template = Handlebars.compile($("#home-tpl").html());
+HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
--- js/main.js
+++ js/main.js
@@ -2,16 +2,6 @@
 var app = {
 
     //
-    findByName: function() {
-        console.log('findByName');
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-
-        });
-    },
-
-    //
     showAlert: function(message, title){
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -20,24 +10,12 @@ var app = {
         }
     },
 
-    // Add home 
-    renderHomeView: function(){
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName,this));
-    }, 
 
     //
     initialize: function() {
         var self = this;
-                
-        // Compile templates
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
-        // Initialize
         this.store = new MemoryStore(function() {   
-            self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
     }
 
