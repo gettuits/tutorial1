1937587c13a3007fbbbf464929cc5bdce7db6cb6
gettuits <gettuits@gmail.com>
2013-08-28 13:44:22 +0200

    Using the Contacts API

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -5,6 +5,7 @@ var EmployeeView = function(employee) {
 	this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
 
     // Render function
@@ -13,9 +14,10 @@ var EmployeeView = function(employee) {
     	return this;
 	};	
 
+	// Using the Location API
 	this.addLocation = function(event) {
 	    event.preventDefault();
-	    console.log('addLocation');
+	    console.log('addLocation by HTML5');
 	    navigator.geolocation.getCurrentPosition(
 	        function(position) {
 	            $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
@@ -23,6 +25,24 @@ var EmployeeView = function(employee) {
 	        function() {
 	            alert('Error getting location');
 	        });
+	    return false;
+	};
+
+	// Using the Contacts API
+	this.addToContacts = function(event) {
+	    event.preventDefault();
+    	console.log('addToContacts');
+	    if (!navigator.contacts) {
+	        app.showAlert("Contacts API not supported", "Error");
+	        return;
+	    }
+	    var contact = navigator.contacts.create();
+	    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+	    var phoneNumbers = [];
+	    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+	    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+	    contact.phoneNumbers = phoneNumbers;
+	    contact.save();
 	    return false;
 	};
  	
